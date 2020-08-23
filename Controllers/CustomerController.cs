825987ba2e3dca5726a5d1f3291c1aa886690151
EmilyStieben <emily.stieben@gmail.com>
2020-08-23 16:25:36 -0400

    added initial files

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using dataTable.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace dataTable.Content
+{
+    public class CustomerController : Controller
+    {
+        private List<Customer> Customers = new List<Customer>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            CustomerGet();
+            return View(Customers.OrderBy(c => c.ItemID).ToList());
+        }
+        private void CustomerGet()
+        {
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(Server.MapPath("~/Content/customer.json")))
+            {
+                Customers = JsonConvert.DeserializeObject<List<Customer>>(sr.ReadToEnd());
+            }
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dataTable.Models
+{
+    public class Customer
+    {
+        public int ItemID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+    }
+}
